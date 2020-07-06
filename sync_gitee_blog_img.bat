574b704ffb4b88ce3711bc4dbd19c7ba8f4f513e
shaofeer <shaofeer@163.com>
2020-07-06 16:42:07 +0800

    sync

--- sync_gitee_blog_img.bat
+++ sync_gitee_blog_img.bat
@@ -3,19 +3,25 @@
 set targetDir = E:\project\web\person\BlogImg
 
 
+echo 
 
 echo "start======================%date%%time%" 
 
-
+E:
 cd %targetDir%
 
 
 git pull
 
 
+echo "sync success %date%%time%" >> ./log/sync.log
 
 
+git add ./*
+git commit -m "sync"
+git push
 
+echo "end======================%date%%time%" 
 
 
 
